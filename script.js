7ac734e96f3dcd44845ddad976bbc3e6156f2934
ju <106085321+saturninhoo@users.noreply.github.com>
2024-06-13 14:12:04 -0300

    mais pergunta

--- script.js
+++ script.js
@@ -14,7 +14,28 @@ const perguntas = {
                 texto: "não",
                 afirmacao: "com a mudança climatica sendo muito forte pode acontecer de se perder todo cultivo e com isso perder os inventimentos na lavoura"
             }
+
+            enunciado: "A agricultura pode prejudicar a saúde?"
+        alternativas:{
+                texto: "sim",
+                afirmacao: "realmente, pelo cultivo ter muitos agrotoxicos que podem prejudicar a saúde e meio ambiente, futuramente desencadeando mais problemas"
+            },
+            {
+                texto: "não",
+                afirmacao: "existem muitos agricultores familiares que não utilizam dos agrotoxicos ou usam pouco dele"
+            }
+
+            enunciado: "A agricultura prejudica o meio ambiente?"
+        alternativas:{
+                texto: "sim",
+                afirmacao: "com o desmatamento de arvores e gástoxicos que são liberado, com o gasto de petroleo para o combustivel de maquinas e tratores"
+            },
+            {
+                texto: "não",
+                afirmacao: "mesmo com o possivel desmatamento, a agricultura cuida do solo com minerais, sais e ureia, controlando o solo acido e o deixando fertil"
+            }
         },
+
 
 let atual = 0;
 let perguntaAtual;
