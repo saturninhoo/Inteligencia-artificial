50183d062fd3f88c191785ff7722bbc51b9ccd0b
ju <106085321+saturninhoo@users.noreply.github.com>
2024-07-24 13:31:16 -0300

    achei

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ const perguntas = [
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
@@ -64,19 +65,24 @@ function mostraPergunta() {
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
-     
-
-
-        function respostaSelecionada(opcaoSelecionada) {
-            const afirmacoes = opcaoSelecionada.afirmacao;
-            historiaFinal += afirmacoes + " ";
-            atual++;
-            mostraPergunta();
-        }
-        
-        function mostraResultado() {
-            caixaPerguntas.textContent = "Em 2049...";
-            textoResultado.textContent = historiaFinal;
-            caixaAlternativas.textContent = "";
-        }
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
